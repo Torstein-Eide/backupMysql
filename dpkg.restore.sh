626f6d072c53366f8d4072f83a96c16764650496
Eideen <Torsteine@gmail.com>
2018-07-15 01:32:08 +0200

    Update dpkg.restore.sh

--- dpkg.restore.sh
+++ dpkg.restore.sh
@@ -9,3 +9,10 @@ apt-get install dselect
 dselect update
 dpkg --set-selections < ./Package.list
 apt-get dselect-upgrade -y
+
+# You may have to update dpkg's list of available packages or\
+#it will just ignore your selections (see this debian bug for more info).\
+#You should do this before sudo dpkg --set-selections < ~/Package.list, like this:
+#apt-cache dumpavail > ./temp_avail
+#sudo dpkg --merge-avail ./temp_avail
+#s3rm ./temp_avail
