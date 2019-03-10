2727e0080b7f56ea57faa631fe8fb400969ea294
Eideen <Torsteine@gmail.com>
2019-03-10 21:54:13 +0100

    removed some leading text from $0, some clean up.

--- db_backup_daily.sh
+++ db_backup_daily.sh
@@ -1,20 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 # Shell script to backup MySQL database
 set -euo pipefail
 IFS=$'\n\t'
-export scriptname="$0"
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd $DIR
 
 ############################################
 ###Remember to edit ./db_backup_common.sh###
 ############################################
 
 # How many days old files must be to be removed
-export DAYS=31
+DAYS=31
 # Backup Dest directory
 export DEST="$HOME/db_backup_daily" # edit me
+
+
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
+
+#  Used for Temp folder
+scriptname="$0"
+scriptname=${scriptname::-3}
+export scriptname=${scriptname:2}
 
 ./db_backup_common.sh
 
@@ -23,4 +30,3 @@ find $DEST -mtime +$DAYS -exec rm -f {} \;
 
 echo ""
 echo "MySQL backup is completed"
-
--- db_backup_horly.sh
+++ db_backup_horly.sh
@@ -1,33 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 # Shell script to backup MySQL database
 set -euo pipefail
 IFS=$'\n\t'
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-cd $DIR
-
-#Used for Temp folder
-export scriptname="$0"
-
-############################################
-###Remember to edit ./db_backup_common.sh###
-############################################
-
+##############################################
+### Remember to edit ./db_backup_common.sh ###
+##############################################
 
 # How many days old files must be to be removed
 HOURS=24
-MIN=$(expr $HOURS \* 60)
-
 
 ## Backup Dest directory
 export DEST="$HOME/db_backup_hourly" # edit me
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd $DIR
+#Used for Temp folder
+scriptname="$0"
+scriptname=${scriptname::-3}
+export scriptname=${scriptname:2}
+
 ./db_backup_common.sh
 
+# Convert hours inn to minutts for find
+MIN=$(expr $HOURS \* 60)
 # Remove old files
 find $DEST -mmin +$MIN -exec rm -f {} \;
 
 echo ""
 echo "MySQL backup is completed"
-
-
